e7805ca8ae93078ce46ce8aba5f6047e001f0580
Tokuya Kameshima <kametoku@gmail.com>
2022-01-13 01:46:42 +0900

    toast: escape message if parameter raw-p is nil.

--- src/modules/toast.lisp
+++ src/modules/toast.lisp
@@ -5,14 +5,16 @@
 
 (in-package #:fomantic.modules)
 
-(defun toast (&rest args &key message title show-progress class-progress
+(defun toast (&rest args &key message raw-p title show-progress class-progress
                            class position newest-on-top horizontal display-time
                            progress-up show-icon show-image class-image close-icon
                            compact)
-  (declare (ignorable message title show-progress class-progress
+  (declare (ignorable title show-progress class-progress
                       class position newest-on-top horizontal display-time
                       progress-up show-icon show-image class-image close-icon
                       compact))
+  (setf (getf args :message)
+        (if raw-p message (spinneret::escape-to-string message)))
   (let ((parameters (make-init-parameter args)))
 ;;     (log:debug parameters)
     (funcall #'js-method nil 'toast parameters)))
