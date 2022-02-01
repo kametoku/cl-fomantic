804955f075561729d65d54a85a01a681b2a753b3
Tokuya Kameshima <kametoku@gmail.com>
2022-02-01 10:24:43 +0900

    calendar: Set the value on change. Init parameter name changed.
    (fui.module:value calendar-widget) returns the latest value on
    calendar widget now.
    
    Parameter of `make-calendar-widget' has changed as below:
    - initial-date -> value
    - id -> name
    - start-calendar-id -> start-calendar-name
    - end-calendar-id -> end-calendar-name

--- src/modules/calendar.lisp
+++ src/modules/calendar.lisp
@@ -6,27 +6,17 @@
 
 (defwidget calendar-widget (module-widget)
   ;; type ::= "datetime" | "date" | "time" | "month" | "year"
-  ((type :initarg :type
-         :initform "date"
-         :reader calendar-type)
-   (initial-date :initarg :initial-date
-                 :initform nil
-                 :reader initial-date)
-   (start-calendar-id :initarg :start-calendar-id
+  ((type :initarg :type :initform "date" :reader calendar-type)
+   (value :initarg :value :initform nil :accessor value)
+   (start-calendar-name :initarg :start-calendar-name
+                        :initform nil
+                        :reader start-calendar-name)
+   (end-calendar-name :initarg :end-calendar-name
                       :initform nil
-                      :reader start-calendar-id)
-   (end-calendar-id :initarg :end-calendar-id
-                    :initform nil
-                    :reader end-calendar-id)
-   (action :initarg :action
-           :initform nil
-           :reader action)
-   (placeholder :initarg :placeholder
-                :initform "Date"
-                :reader placeholder)
-   (icon :initarg :icon
-         :initform "calendar icon"
-         :reader icon)))
+                      :reader end-calendar-name)
+   (action :initarg :action :initform nil :reader action)
+   (placeholder :initarg :placeholder :initform "Date" :reader placeholder)
+   (icon :initarg :icon :initform "calendar icon" :reader icon)))
 
 (defmethod default-div-class ((widget calendar-widget))
   "ui calendar")
@@ -37,9 +27,9 @@
 (defmethod init-parameters ((widget calendar-widget))
   ;; https://fomantic-ui.com/modules/calendar.html#/settings
   (list 'type (calendar-type widget)
-        'initial-date (initial-date widget)
-        'start-calendar (js-get-element-by-id (start-calendar-id widget))
-        'end-calendar (js-get-element-by-id (end-calendar-id widget))
+        'initial-date (value widget)
+        'start-calendar (js-get-element-by-id (start-calendar-name widget))
+        'end-calendar (js-get-element-by-id (end-calendar-name widget))
         'formatter '(ps:create
                      date (lambda (date)
                             (let ((day (ps:chain
@@ -51,21 +41,32 @@
                                   (year (ps:chain
                                          date (get-full-year))))
                               (+ year "-" month "-" day))))))
-  
+
+(defmethod name ((widget calendar-widget))
+  (reblocks/widgets/dom:dom-id widget))
+
+(defmethod onchange ((widget calendar-widget) &key value)
+  (setf (value widget) value))
+
 (defmethod reblocks/widget:render ((widget calendar-widget))
   (with-html
     ;; <div class="ui calendar" ... </div>
     (:div :class "ui input left icon"
           (:i :class "calendar icon")
           (:input :type "text"
-                  :name (reblocks/widgets/dom:dom-id widget)
-                  :value (initial-date widget)
+                  :name (name widget)
+                  :value (value widget)
+                  :onchange (reblocks-parenscript:make-js-handler
+                             :lisp-code ((&key value &allow-other-keys)
+                                         (onchange widget :value value))
+                             :js-code ((event)
+                                       (ps:create :value (ps:@ event target value))))
                   :autocomplete "off"
                   :placeholder (placeholder widget)))))
 
 (defun make-calendar-widget (&rest args
-                             &key id type initial-date start-calendar-id
-                               end-calendar-id action placeholder icon)
-  (declare (ignore type initial-date start-calendar-id
-                   end-calendar-id action placeholder icon))
-  (apply #'make-instance 'calendar-widget :dom-id id args))
+                             &key name type value start-calendar-name
+                               end-calendar-name action placeholder icon)
+  (declare (ignore type value start-calendar-name
+                   end-calendar-name action placeholder icon))
+  (apply #'make-instance 'calendar-widget :dom-id name args))
